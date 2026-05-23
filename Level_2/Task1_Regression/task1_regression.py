f2ebe132f132da400aa7b327b924d47ebc72fd6b
senz-code <senzongwenyapro@gmail.com>
2026-05-23 20:58:44 +0200

    Update docstring in task1_regression.py
    Removed dataset description from the docstring.

--- Level_2/Task1_Regression/task1_regression.py
+++ Level_2/Task1_Regression/task1_regression.py
@@ -2,7 +2,6 @@
 =============================================================
 Codveda Technology - Data Analytics Internship
 Level 2 | Task 1: Regression Analysis
-Dataset : House Prices CSV (Boston-style)
 =============================================================
 """
 
